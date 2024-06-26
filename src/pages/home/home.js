056b197d0aae320d4e40db64dc83089fe5e96156
7SilviaT <eagles8485@gmail.com>
2024-06-26 10:50:49 -0500

    feat: navigate to store event in carrusel

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -7,35 +7,39 @@ const img = document.querySelectorAll("#imgs img");
 let imageIndex = 0;
 
 function run() {
-    imageIndex++;
-    changeImage();
+  imageIndex++;
+  changeImage();
 }
 
 function changeImage() {
-    if (imageIndex > img.length - 3) {
-        imageIndex = 0;
-    } else if (imageIndex < 0) {
-        imageIndex = img.length - 3;
-    }
-    const translateXValue = -(imageIndex * (100 / 3));
-    imgs.style.transform = `translateX(${translateXValue}%)`;
+  if (imageIndex > img.length - 3) {
+    imageIndex = 0;
+  } else if (imageIndex < 0) {
+    imageIndex = img.length - 3;
+  }
+  const translateXValue = -(imageIndex * (100 / 3));
+  imgs.style.transform = `translateX(${translateXValue}%)`;
 }
 
 function resetInterval() {
-    clearInterval(interval);
-    interval = setInterval(run, 2000);
+  clearInterval(interval);
+  interval = setInterval(run, 2000);
 }
 
 let interval = setInterval(run, 2000);
 
 rightBtn.addEventListener("click", () => {
-    imageIndex++;
-    changeImage();
-    resetInterval();
+  imageIndex++;
+  changeImage();
+  resetInterval();
 });
 
 leftBtn.addEventListener("click", () => {
-    imageIndex--;
-    changeImage();
-    resetInterval();
+  imageIndex--;
+  changeImage();
+  resetInterval();
 });
+
+function navigateToStore() {
+  window.location.href = "pages/store/store.html";
+}
